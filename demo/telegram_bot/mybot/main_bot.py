d815766ec5a2d4f6e0409ea3b853602572f27947
guangrei <20879950+guangrei@users.noreply.github.com>
2023-12-25 20:13:46 +0700

    improve demo/telegram_bot code

--- demo/telegram_bot/mybot/main_bot.py
+++ demo/telegram_bot/mybot/main_bot.py
@@ -15,14 +15,14 @@ def start_nim():
 
 
 @chatbot.add_command("/python_playground", "run python playground!", strict=True)
-def start_nim():
+def start_python():
     message = chatrouter.data_user
     db.update_user_session(message.from_user.id, "python_playground")
     return chatrouter.util.get_func("python_playground", "/start")()
 
 
 @chatbot.add_command("/go_playground", "run go playground!", strict=True)
-def start_nim():
+def start_go():
     message = chatrouter.data_user
     db.update_user_session(message.from_user.id, "go_playground")
     return chatrouter.util.get_func("go_playground", "/start")()
