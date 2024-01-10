6973838d4400cba1a5ea88466050f638e1953485
guangrei <20879950+guangrei@users.noreply.github.com>
2024-01-10 22:12:06 +0700

    add midleware

--- chatrouter.py
+++ chatrouter.py
@@ -7,7 +7,7 @@ Simple but useful router for chatbot.
 """
 
 __author__ = "guangrei"
-__version__ = "v1.0.1"
+__version__ = "v1.0.2"
 
 _data: dict = {}  # chatrouter storage
 data_user: Any = None  # data user storage
@@ -64,7 +64,8 @@ class group:
             if self._async and not inspect.iscoroutinefunction(func):
                 raise ValueError("function must be awaitable!")
             elif not self._async and inspect.iscoroutinefunction(func):
-                raise ValueError("coroutine function is not supported in synchronous mode!")
+                raise ValueError(
+                    "coroutine function is not supported in synchronous mode!")
             _data[self.id][method] = {}
             _data[self.id][method]["callback"] = func
             _data[self.id][method]["description"] = description
@@ -80,9 +81,25 @@ class group:
             if self._async and not inspect.iscoroutinefunction(func):
                 raise ValueError("function must be awaitable!")
             elif not self._async and inspect.iscoroutinefunction(func):
-                raise ValueError("coroutine function is not supported in synchronous mode!")
+                raise ValueError(
+                    "coroutine function is not supported in synchronous mode!")
             _data[self.id]["__default__"] = {}
             _data[self.id]["__default__"]["callback"] = func
+            return func
+        return dec
+
+    def midleware(self) -> Callable:
+        """
+        fungsi untuk menambahkan midleware
+        """
+        def dec(func: Callable) -> Callable:
+            if self._async and not inspect.iscoroutinefunction(func):
+                raise ValueError("function must be awaitable!")
+            elif not self._async and inspect.iscoroutinefunction(func):
+                raise ValueError(
+                    "coroutine function is not supported in synchronous mode!")
+            _data[self.id]["__midleware__"] = {}
+            _data[self.id]["__midleware__"]["callback"] = func
             return func
         return dec
 
@@ -228,9 +245,13 @@ def run(route: group, msg: str) -> Union[str, None]:
     """
     ini adalah fungsi utama untuk interpretasi chatrouter
     """
+    if "__midleware__" in _data[route.id]:
+        coba = _data[route.id]["__default__"]["callback"]()
+        if coba is not True:
+            return coba
     if len(msg):
         for k, v in _data[route.id].items():
-            if k != "__default__":
+            if k not in ("__default__", "__midleware__"):
                 args = util._route(k, msg, v["strict"])
                 if args is not False:
                     return v["callback"](*args)
@@ -244,9 +265,13 @@ async def async_run(route: group, msg: str) -> Union[str, None]:
     """
     ini adalah versi async dari function run
     """
+    if "__midleware__" in _data[route.id]:
+        coba = await _data[route.id]["__default__"]["callback"]()
+        if coba is not True:
+            return coba
     if len(msg):
         for k, v in _data[route.id].items():
-            if k != "__default__":
+            if k not in ("__default__", "__midleware__"):
                 args = await util._async_route(k, msg, v["strict"])
                 if args is not False:
                     ret = await v["callback"](*args)
@@ -269,7 +294,8 @@ def add_command(route: str, description: str = "", strict: bool = False, group_r
         if asynchronous and not inspect.iscoroutinefunction(func):
             raise ValueError("function must be awaitable!")
         elif not asynchronous and inspect.iscoroutinefunction(func):
-            raise ValueError("coroutine function is not supported in synchronous mode!")
+            raise ValueError(
+                "coroutine function is not supported in synchronous mode!")
         _data[group_route][method] = {}
         _data[group_route][method]["callback"] = func
         _data[group_route][method]["description"] = description
@@ -288,9 +314,28 @@ def add_default_command(group_route: str = "main", asynchronous: bool = False) -
         if asynchronous and not inspect.iscoroutinefunction(func):
             raise ValueError("function must be awaitable!")
         elif not asynchronous and inspect.iscoroutinefunction(func):
-            raise ValueError("coroutine function is not supported in synchronous mode!")
+            raise ValueError(
+                "coroutine function is not supported in synchronous mode!")
         _data[group_route]["__default__"] = {}
         _data[group_route]["__default__"]["callback"] = func
+        return func
+    return dec
+
+
+def midleware(group_route: str = "main", asynchronous: bool = False) -> Callable:
+    """
+    fungsi untuk menambahkan midleware secara cepat
+    """
+    group(group_route, asynchronous=asynchronous)
+
+    def dec(func: Callable) -> Callable:
+        if asynchronous and not inspect.iscoroutinefunction(func):
+            raise ValueError("function must be awaitable!")
+        elif not asynchronous and inspect.iscoroutinefunction(func):
+            raise ValueError(
+                "coroutine function is not supported in synchronous mode!")
+        _data[group_route]["__midleware__"] = {}
+        _data[group_route]["__midleware__"]["callback"] = func
         return func
     return dec
 
